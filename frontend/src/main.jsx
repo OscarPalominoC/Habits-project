32a171e24018ab50c4e9a909d4a9e3980109ad8d
Oscar Palomino <oscar_epalomino@soy.sena.edu.co>
2026-04-20 19:38:29 -0500

    configuración Vite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/habits">
+    <BrowserRouter basename="/habits/">
       <App />
     </BrowserRouter>
   </StrictMode>,
